ebb15f5aff58b2887af25be52036e1482e1c0909
Лера <valeri.gorde@mail.ru>
2022-10-04 11:51:11 +0300

    Добавьте файлы проекта.

---
+++ FinalTaskLINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace FinalTaskLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));            
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var allStudents = classes.SelectMany(x => x.Students).ToArray();
+
+            return allStudents;
+        }
+
+    }
+}
